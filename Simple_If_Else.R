8796d6597d6e5588aca1f7ba39a51db90e1b8844
Devesh Khandelwal <devesh1412@gmail.com>
2020-01-01 21:46:46 -0800

    Simple conditional in R

---
+++ Simple_If_Else.R
@@ -0,0 +1,18 @@
+input <- 75
+
+if(input >= 1 & input < 60)
+{
+  print("Rotten!!")
+} else if (input >=60 & input < 75)
+    {
+      print("Fresh!!")
+     }  else if (input >= 75)
+        { 
+  
+        print("Certified Fresh")
+        } else {
+            print("Enter a number between 0 and 100")
+           }
+
+
+     
